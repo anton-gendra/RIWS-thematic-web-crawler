893bbf7b3becc67c498b20b0284b2ea550794481
anton <gendra.job@gmail.com>
2022-09-30 17:27:01 +0200

    Added file with all links to resource pages. Added line to gitignore

--- crawler_riws/crawler_riws/settings.py
+++ crawler_riws/crawler_riws/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'component_search'
+
+    
+    'component_search.apps.ComponentSearchConfig',
 ]
 
 MIDDLEWARE = [
