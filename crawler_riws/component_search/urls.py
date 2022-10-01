3ca9af218f4ddf60534d2e23e567f6dd4bf3f259
anton <gendra.job@gmail.com>
2022-10-01 21:25:57 +0200

    Added templates home.html and index.html

--- crawler_riws/component_search/urls.py
+++ crawler_riws/component_search/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from component_search import views
 
 urls = [
-    path('', views.search, name='main_page')
+    path('', views.search, name='main_page'),
+    path('index.html', views.search, name='main_page')
 ]
--- crawler_riws/component_search/views.py
+++ crawler_riws/component_search/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def search(request):
-    return render(request, 'base.html')
+    context = None
+    if request.GET:
+        context = {
+            'result': request.GET['search-input']
+        }
+
+    return render(request, 'index.html', context)
