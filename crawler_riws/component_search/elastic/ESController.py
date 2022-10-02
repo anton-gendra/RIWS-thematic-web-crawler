c4c054c1edcd47ebdd9e36a1b95ec63d9af7c116
anton <gendra.job@gmail.com>
2022-10-02 20:45:18 +0200

    Created elasticsearch controller class. Added example

---
+++ crawler_riws/component_search/elastic/ESController.py
@@ -0,0 +1,13 @@
+import requests
+import os
+
+class ESController:
+    base_url = "http://elastic:" + os.environ['ES_PASSWORD'] + "@localhost:9200/"
+
+    def get_status(self):
+        return requests.get(self.base_url)
+
+
+if __name__ == '__main__':
+    elastic = ESController()
+    print(elastic.get_status())
