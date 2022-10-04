8d39e97c943c927606bd3ba61e0c071b21eeb0a7
anton <gendra.job@gmail.com>
2022-10-04 19:05:50 +0200

    Added function in elastic controller to create new index

--- crawler_riws/component_search/elastic/ESController.py
+++ crawler_riws/component_search/elastic/ESController.py
@@ -1,13 +1,58 @@
+from typing import Any, Dict, Tuple
+from django.http import Http404
+from elasticsearch import Elasticsearch, BadRequestError
 import requests
 import os
 
+# TODO: Remove
+# Hago este sólo de ejemplo, luego vemos cómo hacemos
+idx = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+    },
+    "mappings": {
+        "properties": {
+            "name": {"type": "text"},
+            "price": {"type": "float"},
+            "characteristics": {
+                "properties": {
+                    "RAM": {"type": "integer"},
+                    "height": {"type": "float"}
+                }
+            }
+        }
+    }
+}
+
+
 class ESController:
     base_url = "http://elastic:" + os.environ['ES_PASSWORD'] + "@localhost:9200/"
+    es = Elasticsearch(base_url)
 
-    def get_status(self):
-        return requests.get(self.base_url)
+    def get_es_status(self) -> Tuple[Tuple[int, str], str]:
+        result = requests.get(self.base_url)
+        return (result.status_code, result.reason), result.content.decode('utf-8')
+
+
+    # To remove an index we can do: curl -X DELETE http://elastic:<password>@localhost:9200/<index_name>
+    def create_index(self, index_name: str, mapping: Dict[str, Any]):
+        try:
+            result = self.es.indices.create(index=index_name, body=mapping)
+        
+        except BadRequestError as e:
+            return e.meta.status, e.error
+
+        return result.body
+
+    def get_index_status(self, index_name: str) -> Tuple[Tuple[int, str], str]:
+        result = requests.get(self.base_url + index_name)
+        return (result.status_code, result.reason), result.content.decode('utf-8')
+
 
 
 if __name__ == '__main__':
     elastic = ESController()
-    print(elastic.get_status())
+    print(elastic.get_es_status())
+    print(elastic.create_index("test_index", idx))
+    print(elastic.get_index_status("test_index"))
