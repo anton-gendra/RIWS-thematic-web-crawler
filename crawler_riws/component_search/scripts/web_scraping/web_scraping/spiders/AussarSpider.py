2acf39d496b9922c8276e91c94685bf9ba9b7a19
Juan <j.justo.armesto@udc.es>
2022-11-10 20:36:03 +0100

    feat: image in parse component

--- crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/AussarSpider.py
+++ crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/AussarSpider.py
@@ -1,3 +1,4 @@
+from gc import callbacks
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
@@ -27,11 +28,23 @@ class AussarSpider(CrawlSpider):
             'refrigeracion/ventiladores-caja-cpu',
             'redes/tarjetas-de-red-y-wifi',
             'sistemas-operativos'
-        ]), callback='parse_component'),
+        ]), callback='parse_list'),
     )
 
-    def parse_component(self, response):
-        components = response.css("h3.product-title")
+    def parse_component(self, response, component):
+        price = response.css("span.current-price-value::attr(content)").get()
+        brand = response.css("div.product-manufacturer a::text").get()
+        source = 'aussar'
+        yield component
+
+    def parse_list(self, response):
+        category = response.css("h1.category-name::text").get()
+
+        components = response.css("div.ajax_block_product")
         for component in components:
-            name = component.css("a::text").get()
-            yield Component(name=name)
+            name = component.css("h3.product-title a::text").get()
+            link = component.css("h3.product-title a::attr(href)").get()
+            image = component.css("img.img-fluid::attr(src)").get()
+
+            arg = {'component': Component(name=name, category=category, link=link, image=image)}
+            yield response.follow(link, callback=self.parse_component, cb_kwargs=arg)
