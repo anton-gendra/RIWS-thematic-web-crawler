61b6d58566520e75e28fde1ee7f9916f9c656e17
Adrián Delfín Mella Castelo <adrian.mcastelo@udc.es>
2022-11-11 16:08:23 +0100

    Added PcMontajes spider

--- crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/PcMontajesSpider.py
+++ crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/PcMontajesSpider.py
@@ -26,11 +26,24 @@ class PcMontajesSpider(CrawlSpider):
             '55-componentes-lectores-de-tarjetas',
             '89-conectividad',
             '115-componentes-tarjetas-de-sonido'
-        ]), callback='parse_component'),
+        ]), callback='parse_list'),
     )
 
-    def parse_component(self, response):
+    def parse_component(self, response, component):
+        price = response.css('div.content-price span.our_price_display::attr(content)').get()
+        print('precio capullo', price)
+        brand = response.css('span[itemprop="brand"] span::attr(content)').get()
+        print('marca CAPULLO', brand)
+        source = "pcmontajes"
+        yield component
+
+    def parse_list(self, response):
+        category = response.css("span.cat-name::text").get().strip()
+        print(category)
         components = response.css("div.ajax_block_product")
         for component in components:
             name = component.css("a.product-name::text").get().strip()
-            yield Component(name=name)
+            link = component.css("div.product-meta a::attr(href)").get()
+            print('amongus mongo', link)
+            arg = {'component': Component(name=name, category=category)}
+            yield response.follow(link, callback = self.parse_component, cb_kwargs = arg)
