57b7446c53214a36cca3eeec9d866312678c9efe
Adrián Delfín Mella Castelo <adrian.mcastelo@udc.es>
2022-11-11 18:32:12 +0100

    Update spider

--- crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/PcMontajesSpider.py
+++ crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/PcMontajesSpider.py
@@ -30,20 +30,47 @@ class PcMontajesSpider(CrawlSpider):
     )
 
     def parse_component(self, response, component):
-        price = response.css('div.content-price span.our_price_display::attr(content)').get()
-        print('precio capullo', price)
         brand = response.css('span[itemprop="brand"] span::attr(content)').get()
-        print('marca CAPULLO', brand)
-        source = "pcmontajes"
+        component['brand'] = brand
+        price = response.css('div.our_price_display meta[itemprop="price"]::attr(content)').get()
+        component['price'] = price
+        component['source'] = "pcmontajes"
+        rows = response.css('table.table-data-sheet tr')
+        for row in rows:
+            cells = row.css('td')
+            if (cells[0].root.text == 'Peso'):
+                component['weight'] = cells[1].root.text
+            elif (cells[0].root.text == 'Altura'):
+                component['height'] = cells[1].root.text
+            elif (cells[0].root.text == 'Ancho'):
+                component['width'] = cells[1].root.text
+            elif ((cells[0].root.text == 'Memoria máxima') or (cells[0].root.text == 'Memoria interna máxima') ('Memoria máxima' in cells[0].root.text)):
+                component['storing_capacity'] = cells[1].root.text
+            elif ((cells[0].root.text == 'Potencia de diseño térmico (TDP)') or (cells[0].root.text == 'Intervalo de temperatura operativa') or (cells[0].root.text == 'Potencia total')):
+                component['power'] = cells[1].root.text
+            elif ((cells[0].root.text == 'Frecuencia base del procesador') or (cells[0].root.text == 'WLAN velocidad de transferencia de datos, soportada')):
+                component['speed'] = cells[1].root.text
+            elif (cells[0].root.text == 'Frecuencia de entrada AC'):
+                component['latency'] = cells[1].root.text
+            elif (cells[0].root.text == 'Intersección T'):
+                component['max_temperature'] = cells[1].root.text
+            elif (cells[0].root.text == 'Generación del procesador'):
+                component['generation'] = cells[1].root.text
+            elif (cells[0].root.text == 'Socket de procesador'):
+                component['socket'] = cells[1].root.text
+            elif ((cells[0].root.text == 'Interfaz') or (cells[0].root.text == 'Tipo de interfaz')):
+                component['interface'] = cells[1].root.text
+
         yield component
 
     def parse_list(self, response):
-        category = response.css("span.cat-name::text").get().strip()
+        category = response.css("span.cat-name::text").get().strip() if response.css("span.cat-name::text").get() else None
         print(category)
         components = response.css("div.ajax_block_product")
         for component in components:
             name = component.css("a.product-name::text").get().strip()
             link = component.css("div.product-meta a::attr(href)").get()
-            print('amongus mongo', link)
-            arg = {'component': Component(name=name, category=category)}
+            img = component.css("img.replace-2x::attr(src)").get()
+            print('amongus mongo', category)
+            arg = {'component': Component(name=name, category=category, link=link, image = img)}
             yield response.follow(link, callback = self.parse_component, cb_kwargs = arg)
