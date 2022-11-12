1650b80d3fd67726d7525b3495caddedaffea6a4
Juan <j.justo.armesto@udc.es>
2022-11-12 13:28:00 +0100

    Increased the component

--- crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/WipoidSpider.py
+++ crawler_riws/component_search/scripts/web_scraping/web_scraping/spiders/WipoidSpider.py
@@ -28,20 +28,43 @@ class WipoidSpider(CrawlSpider):
     )
 
     def parse_component(self, response, component):
-        #print('RESPONSEEEEEEEEE:',response)
-        #print('COMPONENTTTTTTTT:',component)
-        #print(response.css("section.page-product-box p::text").getall())
-        #print('NAMEEEEEEE:', component["name"])
+        component['source'] = 'wipoid'
         productLabels = response.css("div.prd-reference a::text").getall()
         brand = productLabels[1] if (len(productLabels) == 2) else None
-        print('BRAND:', brand)
+        component['brand'] = brand
         price = response.css("div.our_price_display span::text").get()
-        print('priceeeeeeee:',price)
+        component['price'] = price
         specsList = response.css("section.page-product-box p::text").getall()
-        link = response.url
-        #print('sourceee:', link[8:].split('/')[0])
-        source = 'wipoid'
-        print('listaaaaa:', specsList)
+
+        specsListLen = len(specsList)
+        for i,e in enumerate(specsList):
+            #print('objetooooooooooooo', e)
+            if ('gráfico' in e.lower()) or ('arquitectura' in e.lower()):
+                component['achitecture'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif 'speed' in e.lower():
+                component['speed'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            #elif 'cuda' in e.lower():
+            #    component['cuda'] = None if ((i+1) > specsList) else specsList[i+1]
+            elif ('tamaño de la memoria' in e.lower()) or ('memoria de video' in e.lower()) or ('capacidad' in e.lower()):
+                component['storing_capacity'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif ('tamaño de la tarjeta' in e.lower()) or ('dimensiones' in e.lower()):
+                component['height'] = e
+                component['width'] = e
+            elif ('peso' in e.lower()):
+                component['weight'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif ('fuente' in e.lower()) or ('tdp' in e.lower()) or ('salida total' in e.lower()):
+                component['power'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif ('temperatura' in e.lower()) or ('temp. max' in e.lower()):
+                component['max_temperature'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif ('número de procesador' in e.lower()):
+                component['generation'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif ('interfaz' in e.lower()):
+                component['interface'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif ('latencia' in e.lower()):
+                component['latency'] = None if ((i+1) > specsListLen) else specsList[i+1]
+            elif ('socket' in e.lower()):
+                component['socket'] = e
+            
         yield component
 
     def parse_list(self, response):
