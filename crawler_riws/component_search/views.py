17f204f7f25529885487ecee80a93be67ff83529
Adrián Delfín Mella Castelo <adrian.mcastelo@udc.es>
2022-11-15 23:15:33 +0100

    Added  finsihed web template

--- crawler_riws/component_search/views.py
+++ crawler_riws/component_search/views.py
@@ -14,17 +14,27 @@ def filter(request):
 
     name = None
     category = None
+    min_price = None
+    max_price = None
+    brand = None
 
     if request.GET:
         name = request.GET.get('name')
         category = request.GET.get('category')
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+        brand = request.GET.get('brand')
 
-    components = es.get_component_by_name('SSD') # SSD es solo de prueba borra esto
-    form = FilterForm()
+    components = es.get_component_by_name(name if name else '')
+
     context = {
         'components_categories': COMPONENT_CAREGORIES,
         'brands': BRANDS,
-        'params': name,
+        'name': name,
+        'category': category,
+        'min_price': min_price,
+        'max_price': max_price,
+        'brand': brand,
         'components': components
     }
 
