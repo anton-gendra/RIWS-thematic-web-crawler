cc896c925092e4641fd54ef4058d375dcc0e08ca
Adrián Delfín Mella Castelo <adrian.mcastelo@udc.es>
2022-11-18 00:28:35 +0100

    Added search function to views

--- crawler_riws/component_search/views.py
+++ crawler_riws/component_search/views.py
@@ -5,6 +5,7 @@ from component_search.elastic.ESController import ESController
 from crawler_riws.settings import COMPONENT_CAREGORIES
 from component_search.scripts.web_scraping.web_scraping.spiders.utils.utils import BRANDS
 from django.core.exceptions import ObjectDoesNotExist
+from component_search.scripts.web_scraping.web_scraping.items import Component
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -33,7 +34,22 @@ def filter(request):
         max_price = request.GET.get('max_price')
         brand = request.GET.get('brand')
 
-    components = es.get_component_by_name(name if name else '')
+    args = {
+        "name": name,
+        "category" : category,
+        "price": {
+            "min": min_price,
+            "max": max_price
+        }, 
+        "orQ": [{"key": "brand", "values": [brand]}],
+        "andQ": []
+    }
+
+    elastic_search_data = es.search(args)
+    components = []
+
+    for data in elastic_search_data:
+        components.append(Component(name=data._source.name, price=data._source.price, brand=data._source.brand, source=data._source.source, link=data._source.link, category=data._source.category, image=data._source.image, storing_capacity=data._source.characteristics.storing_capacity, height=data._source.characteristics.height, width=data._source.characteristics.width, wheight=data._source.characteristics.wheight, power=data._source.characteristics.power, speed=data._source.characteristics.speed, latency=data._source.characteristics.latency, max_temperature=data._source.characteristics.max_temperature, year=data._source.characteristics.year, generation=data._source.characteristics.generation, rating=data._source.characteristics.rating, socket=data._source.characteristics.socket, interface=data._source.characteristics.interface, architecture=data._source.characteristics.architecture, cores=data._source.characteristics.cores, threads=data._source.characteristics.threads, type=data._source.characteristics.type))
 
     for component in components:
         try:
